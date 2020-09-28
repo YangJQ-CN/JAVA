8fafdbd32fbede871a806874cfec8f9474ac6a44
YangJQ <128693310@qq.com>
2020-09-28 10:28:03 +0800

    编程测试

---
+++ JAVA/JAVA_01_Java程序员/JAVA_01_基础语法/src/main/java/cn/yjq/Java_基础语法/Main.java
@@ -0,0 +1,12 @@
+package cn.yjq.Java_基础语法;
+
+
+import org.junit.Test;
+
+public class Main {
+
+    @Test
+    public void 集成单元测试() {
+        System.out.println("集成单元测试");
+    }
+}
---
+++ JAVA/JAVA_01_Java程序员/JAVA_01_基础语法/src/main/java/cn/yjq/Java_基础语法/程序结构/程序结构.java
@@ -0,0 +1,43 @@
+package cn.yjq.Java_基础语法.程序结构;
+
+import org.junit.Test;
+
+public class 程序结构 {
+
+    @Test
+    public void 顺序结构() {
+
+    }
+
+    @Test
+    public void 分支结构() {
+        //普通判断
+        if (true) {
+
+        }
+
+        //普通分支
+        if (true) {
+
+        } else {
+
+        }
+
+        //多分支
+        if (true) {
+
+        } else if (true) {
+
+        } else {
+
+        }
+
+        //switch语句
+        switch ('1') {
+            case '1':;;
+            case '2':;;
+            case '3':;;
+            default:;
+        }
+    }
+}
